85cf686ea1cf9e3ef5f2abc57f9fb5ee58d6e166
Laszlo Teveli <tevelee@gmail.com>
2023-11-01 20:00:00 +0100

    Publish HFlowLayout and VFlowLayout

--- Sources/Flow/Internal/HFlowLayout.swift
+++ Sources/Flow/Internal/HFlowLayout.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
-struct HFlowLayout {
+public struct HFlowLayout {
     private let layout: FlowLayout
 
-    init(alignment: VerticalAlignment,
-         itemSpacing: CGFloat?,
-         rowSpacing: CGFloat?) {
+    public init(
+        alignment: VerticalAlignment,
+        itemSpacing: CGFloat?,
+        rowSpacing: CGFloat?
+    ) {
         layout = .horizontal(alignment: alignment,
                              itemSpacing: itemSpacing,
                              lineSpacing: rowSpacing)
@@ -15,15 +17,15 @@ struct HFlowLayout {
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
 extension HFlowLayout: Layout {
-    func sizeThatFits(proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) -> CGSize {
+    public func sizeThatFits(proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) -> CGSize {
         layout.sizeThatFits(proposal: proposal, subviews: subviews, cache: &cache)
     }
 
-    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) {
+    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) {
         layout.placeSubviews(in: bounds, proposal: proposal, subviews: subviews, cache: &cache)
     }
 
-    static var layoutProperties: LayoutProperties {
+    public static var layoutProperties: LayoutProperties {
         var properties = LayoutProperties()
         properties.stackOrientation = .horizontal
         return properties
--- Sources/Flow/Internal/VFlowLayout.swift
+++ Sources/Flow/Internal/VFlowLayout.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
-struct VFlowLayout {
+public struct VFlowLayout {
     private let layout: FlowLayout
 
-    init(alignment: HorizontalAlignment,
-         itemSpacing: CGFloat?,
-         columnSpacing: CGFloat?) {
+    public init(
+        alignment: HorizontalAlignment,
+        itemSpacing: CGFloat?,
+        columnSpacing: CGFloat?
+    ) {
         layout = .vertical(alignment: alignment,
                            itemSpacing: itemSpacing,
                            lineSpacing: columnSpacing)
@@ -15,15 +17,15 @@ struct VFlowLayout {
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
 extension VFlowLayout: Layout {
-    func sizeThatFits(proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) -> CGSize {
+    public func sizeThatFits(proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) -> CGSize {
         layout.sizeThatFits(proposal: proposal, subviews: subviews, cache: &cache)
     }
 
-    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) {
+    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: LayoutSubviews, cache: inout ()) {
         layout.placeSubviews(in: bounds, proposal: proposal, subviews: subviews, cache: &cache)
     }
 
-    static var layoutProperties: LayoutProperties {
+    public static var layoutProperties: LayoutProperties {
         var properties = LayoutProperties()
         properties.stackOrientation = .vertical
         return properties
